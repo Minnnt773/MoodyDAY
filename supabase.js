2685ae50667bc4d9dc48d66b88509d10c78d7d34
PrintfM <50163777+Minnnt773@users.noreply.github.com>
2025-07-23 21:28:24 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,47 +1,49 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+import{ createClient } from'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+const supabaseUrl = 'https://cbzvgkpmlagvihiswtpr.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNienZna3BtbGFndmloaXN3dHByIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI1NzIxMDAsImV4cCI6MjA2ODE0ODEwMH0.zEWErfdsjpr5nNLzNiFJhU6M9ft59-VAxCafF9jU8j4'
+const supabase = createClient(supabaseUrl, supabaseKey)
 
-let supabase;
-export function initSupabase() {
-  supabase = createClient(
-    'https://cbzvgkpmlagvihiswtpr.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNienZna3BtbGFndmloaXN3dHByIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI1NzIxMDAsImV4cCI6MjA2ODE0ODEwMH0.zEWErfdsjpr5nNLzNiFJhU6M9ft59-VAxCafF9jU8j4'
-  );
-}
-document.getElementById('login-form').querySelector('form').addEventListener('submit', 
-  
-    async function(e) {
-        e.preventDefault()
-        const email = this.email.value
-        const password = this.password.value
-        const { data, error } = await supabase.auth.signInWithPassword({
-            email,
-            password
-        })
-        if(error) {
-        alert('Login error: '+ error.message)
-        } else{
-        alert('Login successful!')
+import { supabase } from './supabase.js'
+
+// สมัครสมาชิก
+document.querySelector('Register-form').addEventListener('submit', async (e) => {
+    e.preventDefault();
     
-// Redirect or update UI as needed
-}
+    const name = e.target.name.value
+    const email = e.target.email.value
+    const password = e.target.password.value
+
+    const { data, error } = await supabase.auth.signUp({
+        email,
+        password,
+        options: {
+            data: { name }  // เก็บ name เพิ่มเข้าไปใน metadata
+        }
+    })
+
+    if (error) {
+        alert('Register failed: ' + error.message)
+    } else {
+        alert('Register successful! Please check your email to confirm.')
+    }
 })
 
+// ล็อกอิน
+document.querySelector('login-form').addEventListener('submit', async (e) => {
+    e.reventDefault();
 
-document.getElementById('Register-form').querySelector('form').addEventListener('submit', 
+    const email = e.target.email.value
+    const password = e.target.password.value
 
-    async function(e){
-        e.preventDefault()
-        const name = this.name.value
-        const email = this.email.value
-        const password = this.password.value
-        const { user, error } = await supabase.auth.signUp({
-            email,
-            password,
-            options: { data: { name }}
-         })
-        if(error) {
-            alert('Registration error: '+ error.message)
-    } else{
-    alert('Registration successful! Please check your email to confirm.')
-  }
+    const { data, error } = await supabase.auth.signInWithPassword({
+        email,
+        password
+    })
+
+    if (error) {
+        alert('Login failed: ' + error.message)
+    } else {
+        alert('Login successful!')
+        console.log('User info:', data.user)
+    }
 })
