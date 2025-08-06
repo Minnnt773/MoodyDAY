a2c6d0b9bb1528c69cf0c9e28b29aad035c9381b
PrintfM <50163777+Minnnt773@users.noreply.github.com>
2025-08-06 08:27:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 import { supabase } from '/supabase.js'
 
-function showForm(formId){
-    document.querySelectorAll(".form-box").forEach(form => form.classList.remove("active"));
-    document.getElementById(formId).classList.add("active");
-}
 
 
 // สมัครสมาชิก
